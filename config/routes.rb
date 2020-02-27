761c92f7d277d885db17c714003a9038241d751b
miho takeyama <miho1ara@gmail.com>
2020-02-27 19:47:17 +0900

    resoucesあり

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get "/users" => "users#index"
-  get "/users/new" => "users#new"
-  post "/users" => "users#create"
-  get "/users/:id/edit" => "users#edit"
-  patch "users/:id" => "users#update"
-  delete "/users/:id" => "users#destroy"
+  resources :users
 end
